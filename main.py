33a474d549b2f0a4959b99ab84ae1b04c5df11a9
Rayane Benessaidi <rbeness@Rayanes-MacBook-Pro.local>
2025-09-04 12:26:44 +0200

    add

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ URLS = [
     {
          "name": "Olympique de Marseille",
          "url": "https://billetterie.om.fr/fr/",
-         "match_keywords": [ "Liverpool","Atalanta","Newcastle","Champions"],
+         "match_keywords": [ "Liverpool","Atalanta","Newcastle"],
     },
 ]
 
